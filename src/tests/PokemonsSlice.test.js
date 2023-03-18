1d6e420bbf07ac8278b38518474f71086b0f3555
anderCM <andercm15@gmail.com>
2023-03-18 02:03:11 -0500

    Babel plugingg for jsx restores

--- src/tests/PokemonsSlice.test.js
+++ src/tests/PokemonsSlice.test.js
@@ -1,58 +1,57 @@
-import pokemonsTypeReducer ,{ filterPokemonsType, getPokemonTypes } from "../redux/pokemons/PokemonSlice";
+import pokemonsTypeReducer, { filterPokemonsType, getPokemonTypes } from '../redux/pokemons/PokemonSlice';
 
 describe('Test suit for pokemonsTypeReducer', () => {
-    it('Should be the same initial state', () => {
-        const initialState = {
-            types:[],
-            error: false,
-            isLoading: false,
-            filteresType: [],
-        };
-        expect(pokemonsTypeReducer(undefined, {})).toEqual(initialState);
+  it('Should be the same initial state', () => {
+    const initialState = {
+      types: [],
+      error: false,
+      isLoading: false,
+      filteresType: [],
+    };
+    expect(pokemonsTypeReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('Should filter pokemon by name', () => {
+    const initialState = {
+      types: [
+        { name: 'fire', url: 'someurl' },
+        { name: 'grass', url: 'someurl' },
+        { name: 'electric', url: 'someurl' },
+      ],
+      error: false,
+      isLoading: false,
+      filteresType: [],
+    };
+
+    const action = filterPokemonsType('elec');
+
+    expect(pokemonsTypeReducer(initialState, action)).toEqual({
+      ...initialState,
+      filteresType: [
+        { name: 'electric', url: 'someurl' },
+      ],
     });
+  });
+  it('Should return fullfiled status', () => {
+    const initialState = {
+      types: [],
+      error: false,
+      isLoading: false,
+      filteresType: [],
+    };
 
-    it('Should filter pokemon by name', () => {
-        const initialState = {
-            types:[
-              {name: 'fire', url: 'someurl'},
-              {name: 'grass', url: 'someurl'},
-              {name: 'electric', url: 'someurl'}
-            ],
-            error: false,
-            isLoading: false,
-            filteresType: [],
-        }
-
-        const action = filterPokemonsType('elec');
-
-        expect(pokemonsTypeReducer(initialState, action)).toEqual({
-            ...initialState,
-            filteresType: [
-                {name: 'electric', url: 'someurl'}
-            ]
-        });
+    const action = {
+      type: getPokemonTypes.fulfilled.type,
+      payload: [
+        { name: 'fire', url: 'https://pokeapi.co/api/v2/type/10/' },
+        { name: 'water', url: 'https://pokeapi.co/api/v2/type/11/' },
+        { name: 'grass', url: 'https://pokeapi.co/api/v2/type/12/' },
+      ],
+    };
+    expect(pokemonsTypeReducer(initialState, action)).toEqual({
+      ...initialState,
+      types: action.payload,
+      filteresType: action.payload,
     });
-    it('Should return fullfiled status', () => {
-        const initialState = {
-            types: [],
-            error: false,
-            isLoading: false,
-            filteresType: [],
-          };
-        
-        const action = {
-            type: getPokemonTypes.fulfilled.type,
-            payload: [
-                { name: 'fire', url:'https://pokeapi.co/api/v2/type/10/' },
-                { name: 'water', url:'https://pokeapi.co/api/v2/type/11/' },
-                { name: 'grass', url:'https://pokeapi.co/api/v2/type/12/' },
-            ]
-        };
-        expect(pokemonsTypeReducer(initialState, action)).toEqual({
-            ...initialState,
-            types: action.payload,
-            filteresType: action.payload,
-        })
-    });
-
-})
+  });
+});
